df3be860685b2953d13bc4720d6a4ad547c81424
Gili Yankovitch <giliy@nyxsecurity.net>
2025-02-22 02:04:39 +0200

    Adding grid locations to lines and pieces

--- scenes/board.gd
+++ scenes/board.gd
@@ -15,8 +15,16 @@ signal line_clear
 var tetrominos: Array[Tetromino] = []
 var row_count = 20
 var column_count = 10
+var piece_size = 48
+var top_y = -456
+var top_x = -240
 var next_tetromino
 var kintsugi: bool = false
+var lines: Array[Line]
+
+func _ready() -> void:
+	for i in range(row_count):
+		lines.append(null)
 
 func spawn_tetromino(type: Shared.Tetromino, is_preview: bool, spawn_position: Vector2):
 	var tetromino_data = Shared.data[type]
@@ -74,38 +82,45 @@ func check_game_over():
 func add_tetromino_to_lines(t: Tetromino):
 	var tetromino_pieces =  t.get_children().filter(func(c): return c is Piece)
 	
-	for piece in tetromino_pieces:
+	for p in tetromino_pieces:
+		var piece = p as Piece
 		var y_position = piece.global_position.y
 		var does_line_for_piece_exists = false
-		for line in get_lines(): 
-			if line.global_position.y == y_position:
-				piece.reparent(line)
-				does_line_for_piece_exists = true
+		var lineIdx = row_count - int((piece.global_position.y - top_y) / piece_size) - 1
+		var line = lines[lineIdx] as Line
+
+		if line == null:
+			line = line_scene.instantiate() as Line
+			lines[lineIdx] = line
+			line.global_position = Vector2(0, y_position)
+			add_child(line)
+		piece.reparent(line)
 		
-		if !does_line_for_piece_exists: 
-			var piece_line = line_scene.instantiate() as Line
-			piece_line.global_position = Vector2(0, y_position)
-			add_child(piece_line)
-			piece.reparent(piece_line)
+		var pieceIdx = int((piece.global_position.x - top_x) / piece_size) 
+		line.setPiece(piece, pieceIdx)
+		print("Setting piece at (" + str(pieceIdx) + ", " + str(lineIdx) + ")")
 
 func get_lines()->Array:
 	return get_children().filter(func(l): return l is Line)
 	
 
 func remove_full_lines():
-	for l in get_lines():
-		var line = l as Line
-		if line.is_line_full(column_count):
-			move_lines_down(line.global_position.y)
+	for i in range(row_count):
+		var line = lines[i] as Line
+		if line != null and line.is_line_full(column_count):
+			move_lines_down(i)
 			line_clear.emit()
 			line.free()
 
-func move_lines_down(y_pos):
-	for line in get_lines(): 
-		if line.global_position.y < y_pos:
-			line.global_position.y += 48
-
-
+func move_lines_down(n):
+	for i in range(n, row_count - 1):
+		if i > n:
+			var line = lines[i] as Line
+			if line != null:
+				line.global_position.y += piece_size
+				
+		lines[i] = lines[i + 1]
+	lines[row_count - 1] = null
 func get_all_pieces():
 	var pieces = []
 	for line in get_lines():
